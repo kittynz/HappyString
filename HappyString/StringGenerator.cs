72df455fe4efd76e87d7f02aec1d654a251f96e1
anyamolchanova <anya.molchanova@rezare.co.nz>
2015-02-17 10:21:43 +1300

    Added project to repo

---
+++ HappyString/StringGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace HappyString
+{
+    public class StringGenerator
+    {
+        private string[] _happyList = new string[] {"smile", "kitten", "ice cream", "holiday"};
+
+        public string Get()
+        {
+            if (!_happyList.Any())
+                return string.Empty;
+
+            var r = new Random(100);
+            var happyIndex = r.Next(0, _happyList.Length - 1);
+            return _happyList[happyIndex];
+        }
+    }
+}
